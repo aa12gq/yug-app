e93e9eb2b3b9a1173d8ba8f2a3dc5326f56aca9a
aa12gq <aa12gq@gmail.com>
2024-11-26 16:04:36 +0800

    :new: feature: add shared_preferences for offline storage

--- lib/global.dart
+++ lib/global.dart
@@ -3,11 +3,15 @@ import 'package:get/get.dart';
 
 import 'common/index.dart';
 import 'common/services/config.dart';
+import 'common/utils/storage.dart';
 
 class Global {
   static Future<void> init() async {
     // plugin initialization
     WidgetsFlutterBinding.ensureInitialized();
+
+    // initialize storage
+    await Storage().init();
 
     // initialize queue
     await Future.wait([
