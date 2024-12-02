b9eb16d6c9fc20861f59615fd49d39fb3791e205
aa12gq <aa12gq@gmail.com>
2024-12-02 16:47:12 +0800

    :new: feat: add badges package for icon notifications

--- lib/common/style/index.dart
+++ lib/common/style/index.dart
@@ -1,3 +1,8 @@
 library style;
 
-// export './xxxx.dart';
+export './colors.dart';
+export './padding.dart';
+export './radius.dart';
+export './theme.dart';
+export './scale.dart';
+export './space.dart';
